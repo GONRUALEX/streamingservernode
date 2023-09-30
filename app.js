1315c3b46185fc52cc0d632d724bc37e8d4c7a95
INSAGS\3006080 <3006080>
2023-09-30 02:03:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const NodeMediaServer = require('node-media-server');
+//C:\Users\3006080\Desktop\streaming\ffmpeg-6.0-full_build\bin
+
+const config = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    mediaroot: './media',
+    allow_origin: '*'
+  },
+  trans: {
+    ffmpeg: 'C:/Users/3006080/Desktop/streaming/ffmpeg-6.0-full_build/bin/ffmpeg.exe',
+    tasks: [
+      {
+        app: 'live',
+        hls: true,
+        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+        hlsKeep: true, // to prevent hls file delete after end the stream
+        dash: true,
+        dashFlags: '[f=dash:window_size=3:extra_window_size=5]',
+        dashKeep: true // to prevent dash file delete after end the stream
+      }
+    ]
+  }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
